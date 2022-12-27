8d5581a267a40740019bf039e74035900402b220
Pavel Shibaev <shibaev.pp@phystech.edu>
2022-12-27 15:01:32 +0400

    Implement SetPort.

--- x/configstore/keeper/msg_server_set_port.go
+++ x/configstore/keeper/msg_server_set_port.go
@@ -3,7 +3,10 @@ package keeper
 import (
 	"context"
 
+	"cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 	"runos_chain/x/configstore/types"
 )
 
@@ -11,7 +14,10 @@ func (k msgServer) SetPort(goCtx context.Context, msg *types.MsgSetPort) (*types
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
-	_ = ctx
-
+	switchConfig, isFound := k.GetHostsDatabase(ctx, msg.Dpid, msg.Mac)
+	if isFound {
+		return nil, errors.Wrap(sdkerrors.ErrInvalidAddress, "Key already exists")
+	}
+	k.SetHostsDatabase(ctx, switchConfig)
 	return &types.MsgSetPortResponse{}, nil
 }
